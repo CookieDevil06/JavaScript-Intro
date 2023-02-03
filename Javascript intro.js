701d6d5c6245068a8e7fdbb5d18c4a1b5ac3157e
CookieDevil06 <123312698+CookieDevil06@users.noreply.github.com>
2023-02-03 16:13:57 -0500

    Add files via upload

---
+++ Javascript intro.js
@@ -0,0 +1,13 @@
+var firstName = "Trent";
+
+var lastName = "Carrion";
+
+var age = 16;
+
+function myNameAlert(firstName, lastName) {
+    alert("My name is " + firstName + " " + lastName + ".");
+}
+
+function myAgeAlert(age) {
+    alert("My age is " + age + ".");
+}
